2ab916f717fcc5fdd3c338d30f8a428a32719df2
Charlotte49 <cheudu49@yahoo..fr>
2020-05-09 13:08:57 +0200

    com

---
+++ TP2/Part1/Circle.js
@@ -0,0 +1,15 @@
+function Circle(radius)
+{
+    this.radius=radius;
+}
+  
+Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.getRadius=function()
+{
+    return this.radius;
+}
+Circle.prototype.draw = function(ctx){
+        ctx.beginPath();
+        ctx.arc(this.getCenter().getX(), this.getCenter().getY(),this.radius, 0, 2 * Math.PI);
+        ctx.stroke();
+    }
